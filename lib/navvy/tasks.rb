3d6a9b431f78a68da03a5ab28e98fd8cc5143d73
Rob Aldred <raldred@gmail.com>
2011-08-05 15:40:31 +0100

    get navvy to write pid files

--- lib/navvy/tasks.rb
+++ lib/navvy/tasks.rb
@@ -8,6 +8,9 @@ namespace :navvy do
 
   desc "Start a Navvy worker."
   task :work => :environment do
+    `echo "#{Process.pid}" > #{Rails.root.join('tmp','pids','navvy.pid')}`  
+    `echo "#{Process.ppid}" > #{Rails.root.join('tmp','pids','navvy.ppid')}`
+    puts "Starting worker..."
     Navvy::Worker.start
   end
 end
